d63c9bf789cd36fd0669992800d9c652ad0e2e1a
mfctanzim00 <69580317+mfctanzim00@users.noreply.github.com>
2022-05-15 03:01:50 +0600

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,35 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int mxn = 1e6;
-int parent[mxn];
-int child_cnt[mxn];
-int find_par(int a)
-{
-    if(parent[a]==a)
-        return parent[a];
-    return find_par(parent[a]);
-}
-void merge_childs(int a,int b)
-{
-    int x=find_par(a);
-    int y=find_par(b);
-    if(x!=y)
-    {
-        if(x>y)
-            swap(x,y);
-        parent[x]=y;
-        child_cnt[y]+=child_cnt[x];
-    }
-}
-Init()
-{
-    for(int i=1; i<mxn; i++)
-    {
-        parent[i] = i;
-    }
-}
-int main()
-{
-    Init();
-}
+struct DSU{
+     int N;
+     vector<int>par, sz;
 
+     void init(int n){
+           N = n;
+           for(int i=0; i<=n; i++){
+               par.push_back(i);
+               sz.push_back(1);
+           }
+     }
+     int root(int r){
+           if(par[r]==r)
+               return r;
+           return par[r]=root(par[r]);
+     }
+
+     void combine(int u, int v){
+           int uPar = root(u);
+           int vPar = root(v);
+
+           if(uPar==vPar)return;
+           if(sz[uPar]<sz[vPar])
+               swap(uPar, vPar);
+
+           sz[uPar]+=sz[vPar];
+           par[vPar]=uPar;
+     }
+};
