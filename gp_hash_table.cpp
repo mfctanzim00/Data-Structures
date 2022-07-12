f6da505fc506a59bbdbfd27a8556f6ed0414ea73
mfctanzim00 <69580317+mfctanzim00@users.noreply.github.com>
2022-07-13 01:41:24 +0600

    Update gp_hash_table.cpp

--- gp_hash_table.cpp
+++ gp_hash_table.cpp
@@ -1,21 +1,21 @@
-// gp_hash_table: https://codeforces.com/blog/entry/60737?fbclid=IwAR0qVSvW6k_IbSftzUvQvVUukgf09Fq6q70W5gWzhYuW0a6UpWtr7h18ZNs
-// https://gist.github.com/Chillee/3bd6ee4429abb4a2e7c19959fb1490ae
-
-
+// blog: https://codeforces.com/blog/entry/62393
 
 //================ gp hash table ======================
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
-struct chash {
-    const long long RANDOM = (long long)(make_unique<char>().get()) ^ chrono::high_resolution_clock::now().time_since_epoch().count();
-    static unsigned long long hash_f(unsigned long long x) {
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
         x += 0x9e3779b97f4a7c15;
         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
         return x ^ (x >> 31);
     }
-    static unsigned hash_combine(unsigned a, unsigned b) { return a * 31 + b; }
-    long long operator()(long long x) const { return hash_f(x)^RANDOM; }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
 };
-gp_hash_table<long long, long long, chash> table;
+// unordered_map<long long, int, custom_hash> safe_map;
+gp_hash_table<long long, int, custom_hash> safe_hash_table;   // Faster
 //=====================================================
